3fc26dd15bd11b92f6ab5aece93ced3efda047d1
Soham <sohamb690@gmail.com>
2024-08-19 02:06:52 +0530

    day 7 1.13

--- test.c
+++ test.c
@@ -6,10 +6,8 @@
 #define Nr 10
 #define Nk 4
 
-// staging the round constants for later use
 static const uint8_t Rcon[10] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36};
 
-// staging the sbox for later ref
 static const uint8_t sbox[256] = {
     // 0     1    2      3     4    5     6     7      8    9     A      B    C     D     E     F
     0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,  // 0//
@@ -47,16 +45,14 @@ static const uint8_t invsbox[256] = {
     0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
     0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
     0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d};
-// the key used
+
 const uint8_t key[16] = {(uint8_t)0x2b, (uint8_t)0x7e, (uint8_t)0x15, (uint8_t)0x16,
                          (uint8_t)0x28, (uint8_t)0xae, (uint8_t)0xd2, (uint8_t)0xa6,
                          (uint8_t)0xab, (uint8_t)0xf7, (uint8_t)0x15, (uint8_t)0x88,
                          (uint8_t)0x09, (uint8_t)0xcf, (uint8_t)0x4f, (uint8_t)0x3c};
 
-// array to store the round keys
 static uint8_t roundkey[176];
 
-// array to store the states
 static uint8_t state[4][4] = {{0x32, 0x88, 0x31, 0xe0},
                               {0x43, 0x5a, 0x31, 0x37},
                               {0xf6, 0x30, 0x98, 0x07},
@@ -92,7 +88,8 @@ key_scheudling(uint8_t *roundkey, uint8_t *key)
         word[1] = roundkey[((i - 1) * 4) + 1];
         word[2] = roundkey[((i - 1) * 4) + 2];
         word[3] = roundkey[((i - 1) * 4) + 3];
-        printf("%d:%x,%x,%x,%x", i, word[0], word[1], word[2], word[3]);
+        printf("%d\n", i);
+        printf("%x,%x,%x,%x\n", word[0], word[1], word[2], word[3]);
 
         // printf("%d,%d,%d,%d", &word[0], &word[1], &word[2], &word[3]);
 
@@ -138,242 +135,7 @@ key_scheudling(uint8_t *roundkey, uint8_t *key)
     }
 }
 
-// Function for xtimes
-uint8_t xtimes(uint8_t a)
+void main()
 {
-    return ((uint16_t)a << 1) ^ (0x1b * (a >> 7 & 0x01));
-}
-
-// Function  for multiplication
-uint8_t multi(uint8_t a, uint8_t b)
-{
-    uint8_t res = 0x00;
-    while (a != 0 && b != 0)
-    {
-
-        res = res ^ (a * ((b & 0x01)));
-
-        a = (a << 1) ^ (0x1b * (a >> 7 & 0x01));
-
-        b = b >> 1;
-    }
-    return res;
-}
-
-// Function For mix columns
-static void mixcolumn(uint8_t s[4][4])
-{
-    uint8_t a, b, c, d;
-    for (int i = 0; i < 4; i++)
-    {
-        a = s[0][i];
-        b = s[1][i];
-        c = s[2][i];
-        d = s[3][i];
-
-        // Explicit defination of the mix column matrix multiplication
-        s[0][i] = multi(0x02, a) ^ multi(0x03, b) ^ c ^ d;
-        s[1][i] = a ^ multi(0x02, b) ^ multi(0x03, c) ^ d;
-        s[2][i] = a ^ b ^ multi(0x02, c) ^ multi(0x03, d);
-        s[3][i] = multi(0x03, a) ^ b ^ c ^ multi(0x02, d);
-    }
-    /* for (int i = 0; i < 4; i++)
-     {
-         printf("%X,%x,%x,%x\n", s[i][0], s[i][1], s[i][2], s[i][3]);
-     }*/
-}
-
-static void invmixcolumn(uint8_t s[4][4])
-{
-    uint8_t a, b, c, d;
-    for (int i = 0; i < 4; i++)
-    {
-        a = s[0][i];
-        b = s[1][i];
-        c = s[2][i];
-        d = s[3][i];
-
-        s[0][i] = multi(0x0e, a) ^ multi(0x0b, b) ^ multi(0x0d, c) ^ multi(0x09, d);
-        s[1][i] = multi(0x09, a) ^ multi(0x0e, b) ^ multi(0x0b, c) ^ multi(0x0d, d);
-        s[2][i] = multi(0x0d, a) ^ multi(0x09, b) ^ multi(0x0e, c) ^ multi(0x0b, d);
-        s[3][i] = multi(0x0b, a) ^ multi(0x0d, b) ^ multi(0x09, c) ^ multi(0x0e, d);
-    }
-}
-
-// Function to add round key
-static void Add_roundkey(uint8_t state[4][4], uint8_t roundkey[176], int round)
-{
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            state[j][i] = state[j][i] ^ roundkey[(round * Nb * 4) + (i * Nb) + j];
-        }
-    }
-}
-
-// Function For the sub byte
-static void Sub_Bytes(uint8_t state[4][4])
-{
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            state[i][j] = sbox[state[i][j]];
-        }
-    }
-}
-
-static void invsub_byte(uint8_t state[4][4])
-{
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            state[i][j] = invsbox[state[i][j]];
-        }
-    }
-}
-
-static void printstate(uint8_t state[4][4])
-{
-    for (int i = 0; i < 4; i++)
-    {
-
-        printf("%X,%x,%x,%x\n", state[i][0], state[i][1], state[i][2], state[i][3]);
-    }
-}
-
-// Function For shiftrow
-
-static void shiftrow(uint8_t state[4][4])
-{
-    uint8_t temp;
-
-    // shift the row 1
-    temp = state[1][0];
-    state[1][0] = state[1][1];
-    state[1][1] = state[1][2];
-    state[1][2] = state[1][3];
-    state[1][3] = temp;
-
-    // Shift row 2
-    temp = state[2][0];
-    state[2][0] = state[2][2];
-    state[2][2] = temp;
-
-    temp = state[2][1];
-    state[2][1] = state[2][3];
-    state[2][3] = temp;
-
-    // shift row 3
-    temp = state[3][3];
-    state[3][3] = state[3][2];
-    state[3][2] = state[3][1];
-    state[3][1] = state[3][0];
-    state[3][0] = temp;
-}
-
-static uint8_t invshiftrow(uint8_t state[4][4])
-{
-    uint8_t temp;
-
-    temp = state[1][3];
-    state[1][3] = state[1][2];
-    state[1][2] = state[1][1];
-    state[1][1] = state[1][0];
-    state[1][0] = temp;
-
-    temp = state[2][0];
-    state[2][0] = state[2][2];
-    state[2][2] = temp;
-
-    temp = state[2][1];
-    state[2][1] = state[2][3];
-    state[2][3] = temp;
-
-    temp = state[3][0];
-    state[3][0] = state[3][1];
-    state[3][1] = state[3][2];
-    state[3][2] = state[3][3];
-    state[3][3] = temp;
-}
-
-static void cipher(uint8_t state[4][4], uint8_t roundkey[176])
-{
-    for (int round = 0; round <= Nr; round++)
-    {
-        printf("Round:%d\n", round);
-        for (int i = 0; i < 4; i++)
-        {
-
-            printf("%x,%x,%x,%x", roundkey[(round * Nb * 4) + (i * Nb) + 0], roundkey[(round * Nb * 4) + (i * Nb) + 1], roundkey[(round * Nb * 4) + (i * Nb) + 2], roundkey[(round * Nb * 4) + (i * Nb) + 3]);
-            printf("\n");
-        }
-    }
-    printf("starting state:\n");
-    printstate(state);
-    Add_roundkey(state, roundkey, 0);
-    printf("after first add round key:\n");
-    printstate(state);
-    for (int round = 1; round <= Nr - 1; round++)
-    {
-        printf("round:%d\n", round);
-        Sub_Bytes(state);
-        printf("after sub bytes:\n");
-        printstate(state);
-
-        shiftrow(state);
-        printf("after shift column:\n");
-        printstate(state);
-
-        mixcolumn(state);
-        printf("after mix column:\n");
-        printstate(state);
-
-        Add_roundkey(state, roundkey, round);
-        printf("after add round key:\n");
-        printstate(state);
-    }
-
-    Sub_Bytes(state);
-    shiftrow(state);
-    Add_roundkey(state, roundkey, Nr);
-}
-
-static void decipher(uint8_t state[4][4], uint8_t roundkey[176])
-{
-    Add_roundkey(state, roundkey, Nr);
-
-    for (int round = Nr - 1; round > 0; round--)
-    {
-        invshiftrow(state);
-        invsub_byte(state);
-        Add_roundkey(state, roundkey, round);
-        invmixcolumn(state);
-    }
-
-    invshiftrow(state);
-    invsub_byte(state);
-    Add_roundkey(state, roundkey, 0);
-}
-
-int main()
-{
-
     key_scheudling(roundkey, key);
-    for (int round = 0; round <= Nr; round++)
-    {
-        printf("Round:%d\n", round);
-        for (int i = 0; i < 4; i++)
-        {
-
-            printf("%x,%x,%x,%x", roundkey[(round * Nb * 4) + (i * Nb) + 0], roundkey[(round * Nb * 4) + (i * Nb) + 1], roundkey[(round * Nb * 4) + (i * Nb) + 2], roundkey[(round * Nb * 4) + (i * Nb) + 3]);
-            printf("\n");
-        }
-    }
-    cipher(state, roundkey);
-    printstate(state);
-    decipher(state, roundkey);
-    printstate(state);
 }
